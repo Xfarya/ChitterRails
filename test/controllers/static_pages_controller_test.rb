4f47cc48a19fa1374e962f22c3b01bc20523d6f6
Farya <76533997+Xfarya@users.noreply.github.com>
2021-11-29 11:50:43 +0000

    added custom helpers

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Chitter"
+    assert_select "title", "Chitter"
   end
 
   test "should get help" do
